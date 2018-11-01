c510a5cd47ba71255de1811b4d7209cd2c417852
Harald Vinje <haraldvinje@gmail.com>
2018-11-01 14:51:34 +0100

    changed variable names according to this microservice

--- src/form_response_collection.py
+++ src/form_response_collection.py
@@ -2,23 +2,22 @@ import os
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
-access_url = os.environ['FORMS_RSPONSE_SERVICE_DATASTORE_1_PORT_27017_TCP_ADDR']
-port = 27017
+access_url = "forms-response-datastore:27017"
 
 
-class FormCollection:
+class FormResponseCollection:
 
     client = None
     db = None
-    form_collection = None
+    form_response_collection = None
 
     def __init__(self):
-        self.client = MongoClient(access_url, port)
+        self.client = MongoClient(access_url)
         self.db = self.client.cidev_db
-        self.form_collection = self.db.form_collection
+        self.form_response_collection = self.db.form_response_collection
 
     def get_form_by_id(self, id):
-        form = self.form_collection.find_one({'id': id})
+        form = self.form_response_collection.find_one({'id': id})
         if not form:
             raise ValueError
 
@@ -26,7 +25,7 @@ class FormCollection:
         return form
 
     def get_all_forms(self):
-        forms = self.form_collection.find({})
+        forms = self.form_response_collection.find({})
         print(forms)
         result = []
         for form in forms:
@@ -35,31 +34,31 @@ class FormCollection:
 
     def add_form(self, form):
 
-        self.form_collection.insert_one(form)
+        self.form_response_collection.insert_one(form)
         return True
 
-    def update_one_form(self, id, updates={'key1': 'arg1', 'key2': 'arg2'}):
+    def update_one_form(self, id, updates): 
         id = {'id': id}
-        if not self.form_collection.find_one(id):
+        if not self.form_response_collection.find_one(id):
             raise ValueError
-        self.form_collection.update_one(id, {'$set': updates})
+        self.form_response_collection.update_one(id, {'$set': updates})
         return True
 
     def delete_one_form(self, name, author):
-        self.form_collection.delete_one({'name': name, 'author': author})
+        self.form_response_collection.delete_one({'name': name, 'author': author})
         return True
 
     def delete_all_forms(self):
-        self.form_collection.delete_many({})
+        self.form_response_collection.delete_many({})
         return True
 
     def delete_form_by_id(self, id):
-        form = self.form_collection.find_one({'id': id})
+        form = self.form_response_collection.find_one({'id': id})
         if not form:
             raise ValueError
-        self.form_collection.delete_one({'id': id})
+        self.form_response_collection.delete_one({'id': id})
         return True
 
 
-form = FormCollection()
+form = FormResponseCollection()
 
