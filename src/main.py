e8011b87fb482a17781f97cb16a242c2e6c71345
Eivind Lie Andreassen <ela4991@gmail.com>
2018-11-11 11:23:24 +0100

    Standardize return objects

--- src/main.py
+++ src/main.py
@@ -10,17 +10,17 @@ form_response_collection = FormResponseCollection()
 @app.route('/', methods=['GET'])
 def get_all_forms():
     forms = form_response_collection.get_all_forms()
-    return jsonify({'all_forms': forms})
+    return jsonify({'data': forms})
 
 
 @app.route('/<id>', methods=['GET'])
 def get_one_form(id):
     try:
         form = form_response_collection.get_form_by_id(id)
-        return jsonify({'form': form})
+        return jsonify({'data': form})
 
     except ValueError:
-        return jsonify({'message': 'form does not exist'}), 404
+        return 'Form does not exist', 404
 
 
 @app.route('/', methods=['POST'])
@@ -28,10 +28,10 @@ def add_form():
     try:
         form = request.get_json()
         form_response_collection.add_form(form)
-        return jsonify({'message': 'Successfully inserted document'}), 201
+        return 'Successfully inserted document', 201
 
     except ValueError:
-        return jsonify({'message': 'Credentials not provied'}), 401
+        return 'Credentials not provied', 401
 
 
 @app.route('/<id>', methods=['PUT'])
@@ -41,7 +41,7 @@ def update_one_form(id):
         form_response_collection.update_one_form(id, updates)
 
     except ValueError:
-        return jsonify({'message': 'form does not exist'}), 404
+        return 'Form does not exist', 404
 
 
 @app.route('/<id>', methods=['DELETE'])
@@ -49,9 +49,9 @@ def delete_one_form(id):
     try:
         form_response_collection.delete_form_by_id(id)
 
-        return jsonify({'message': 'form successfully deleted'}), 200
+        return 'Form successfully deleted', 200
     except ValueError:
-        return jsonify({'message': 'form does not exist'}), 404
+        return 'Form does not exist', 404
 
 
 if __name__ == '__main__':
