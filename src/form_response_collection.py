83fd2b4494b26188a943bf720fa2429dafc7e838
Eivind Lie Andreassen <ela4991@gmail.com>
2018-11-12 12:40:05 +0100

    Change index from workflows to form (derp...)

--- src/form_response_collection.py
+++ src/form_response_collection.py
@@ -14,7 +14,7 @@ class FormResponseCollection:
         self.client = MongoClient(access_url)
         self.db = self.client.cidev_db
         self.form_response_collection = self.db.form_response_collection
-        self.form_response_collection.create_index('workflows', name='workflows-index')
+        self.form_response_collection.create_index('form', name='form-index')
 
     def get_response_by_id(self, rid):
         form = self.form_response_collection.find_one(ObjectId(rid))
